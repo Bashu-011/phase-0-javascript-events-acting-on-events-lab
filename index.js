2ce07025e94a67a7b786b818eda9ea7125957976
Bashu-011 <bashu2609@gmail.com>
2024-01-05 15:17:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    const containerWidth = 400;
+
+    const rightLimit = containerWidth - dodger.offsetWidth;
+
+    if (left < rightLimit){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+  
+  
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
